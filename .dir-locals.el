419a5a4e94fe076a6ff6ec089c9a73edf7c3098a
Vlad Kozin <vladilen.kozin@gmail.com>
2018-06-04 16:52:51 +0100

    Pass deps.edn aliases in .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((clojure-mode
-  (cider-clojure-cli-global-options . "-J'--add-modules=java.xml.bind'")))
+  ;; (cider-clojure-cli-global-options . "-J'--add-modules=java.xml.bind'")
+  (cider-clojure-cli-global-options . "-O:xmlbind -R:lispy")
+  ;; tests are in the same namespace
+  (cider-test-infer-test-ns . (lambda (ns) ns))))
