2ed7ee9348f8acdd8f465b2714d37dbfcd3b5c3c
Vlad Kozin <vladilen.kozin@gmail.com>
2018-06-17 00:41:23 +0100

    Transform bitfinex messages into exch messages

--- src/bitfinex.clj
+++ src/bitfinex.clj
@@ -100,6 +100,46 @@
 #_((juxt ticker base commodity currency) (->Product "tETHBTC"))
 #_((juxt ticker base commodity currency) (->Pair "ETHBTC"))
 
+(defn snapshot->bids-asks [payload]
+  (let [{:keys [bids asks]}
+        (group-by (fn [{:keys [price size]}]
+                    (if (neg? size)
+                      :asks
+                      :bids))
+                  payload)
+
+        bids
+        (map (fn [{:keys [price size]}]
+               [(decimal price)
+                (decimal size)])
+             bids)
+
+        asks
+        (map (fn [{:keys [price size]}]
+               [(decimal price)
+                (decimal (- 0 size))])
+             asks)]
+    {:bids bids
+     :asks asks}))
+
+(defn update->bids-asks [payload]
+  (snapshot->bids-asks payload))
+
+#_
+(=
+  (snapshot->bids-asks
+    [{:price 584.58, :orders 11, :size 65.64632441}
+     {:price 584.51, :orders 1, :size 0.93194317}
+     {:price 584.59, :orders 4, :size -23.39216286}
+     {:price 584.96, :orders 1, :size -7.23746288}
+     {:price 584.97, :orders 1, :size -12.3}])
+
+  '{:bids ([584.58M 65.64632441M]
+           [584.51M 0.93194317M])
+    :asks ([584.59M 23.39216286M]
+           [584.96M 7.23746288M]
+           [584.97M 12.3M])})
+
 (def ^:private CHANNELS (atom {}))
 
 ;;* Responses
@@ -193,21 +233,53 @@
           message)]
 
     (if (spec/invalid? msg)
+
       (do
         (log/error
           "Received message did not conform to spec\n"
           (with-out-str
-            (spec/explain
+            (spec/explain-data
               ::message
               message))))
-      (do
-        (dispatch-msg msg)))))
+
+      (dispatch-msg msg))))
 
 ;;** - dispatch by tag
+
 (defmulti dispatch-msg first)
-(defmethod dispatch-msg :heartbeat [hb] hb)
-(defmethod dispatch-msg :snapshot [sn] sn)
-(defmethod dispatch-msg :update [up] up)
+
+(defn add-ticker-or-drop
+  [[tag {channel :channel
+         :as m}]]
+  (if-let [ticker (get @CHANNELS channel)]
+
+    [tag (assoc m :ticker ticker)]
+
+    (let [reason (str "Ticker for channel " channel " not found")]
+      (log/error reason)
+      [:drop (-> m
+                 (assoc :tag tag)
+                 (assoc :reason reason))])))
+
+(defmethod dispatch-msg :heartbeat
+  [msg]
+  (add-ticker-or-drop msg))
+
+(defmethod dispatch-msg :snapshot
+  [[tag payload]]
+  (add-ticker-or-drop
+    [tag (update
+           payload
+           :snapshot snapshot->bids-asks)]))
+
+(defmethod dispatch-msg :update
+  [[_ {update :update
+       :as m}]]
+  (add-ticker-or-drop
+    [:update
+     (assoc m :update
+            (update->bids-asks [update]))]))
+
 (defmethod dispatch-msg :event [[_ m]]
   (dispatch-event-msg m))
 
@@ -217,9 +289,9 @@
 (defmethod dispatch-event-msg "pong"
   [{ts ::ts cid ::cid :as m}]
   [:pong
-   {:timestamp (timestamp ts)
-    :id cid
-    :original m}])
+   (-> m
+       (assoc :timestamp (timestamp ts))
+       (assoc :id cid))])
 
 (defmethod dispatch-event-msg "info"
   [{event ::event code ::code msg ::msg :as m}]
@@ -236,10 +308,17 @@
               " with unrecognized code " code)
             :drop))
 
+        [orig-tag reason]
+        (when (= tag :drop)
+          [:event
+           (str "Unrecognized code " code
+                " in message")])
+
         payload
-        (-> {}
+        (-> m
             (assoc-some :message msg)
-            (assoc :original m))]
+            (assoc-some :reason reason)
+            (assoc-some :tag orig-tag))]
 
     [tag payload]))
 
@@ -249,27 +328,38 @@
         (ticker
           (->Pair pair))]
 
-    (log/info "Subscribed to ticker " ticker)
     (swap! CHANNELS assoc channel ticker)
+    (log/info "Subscribed to ticker " ticker)
     [:subscribed
-     {:ticker ticker
-      :original m}]))
+     (assoc m :ticker ticker)]))
 
 (defmethod dispatch-event-msg "unsubscribed"
   [{channel ::chanId :as m}]
-  (let [ticker (get @CHANNELS channel)]
-    (log/info "Unsubscribed ticker " ticker)
-    (swap! CHANNELS dissoc channel)
-    [:unsubscribed
-     {:ticker ticker
-      :original m}]))
+  (if-let [ticker (get @CHANNELS channel)]
+
+    (do
+      (swap! CHANNELS dissoc channel)
+      (log/info "Unsubscribed ticker " ticker)
+      [:unsubscribed
+       (assoc m :ticker ticker)])
+
+    (do
+      (let [reason (str "Ticker for channel " channel " not found")]
+        (log/error reason)
+        [:drop
+         (-> m
+             (assoc :tag :unsubscribed)
+             (assoc :reason reason))]))))
 
 (defmethod dispatch-event-msg "error"
   [{code ::code msg ::msg :as m}]
-  (let [tag :error
-        payload {:code code
-                 :message msg
-                 :original m}]
+  (let [tag
+        :error
+
+        payload
+        (-> m
+            (assoc :message msg)
+            (assoc :code code))]
 
     (log/error
       "Received error code from server:"
@@ -279,23 +369,30 @@
 
 (comment
 
-  (receive
-    '(10961
-       [[584.58 11 65.64632441]
-        [584.51 1 0.93194317]
-        [584.59 4 -23.39216286]
-        [584.96 1 -7.23746288]
-        [584.97 1 -12.3]]))
+  (spec/conform
+    :exch/message
+    (receive
+      '(5863
+         [[584.58 11 65.64632441]
+          [584.51 1 0.93194317]
+          [584.59 4 -23.39216286]
+          [584.96 1 -7.23746288]
+          [584.97 1 -12.3]])))
 
+  (receive
+      '(5863
+         [[584.58 11 65.64632441]
+          [584.51 1 0.93194317]
+          [584.59 4 -23.39216286]
+          [584.96 1 -7.23746288]
+          [584.97 1 -12.3]]))
 
   (receive
-    '(10961
+    '(5863
        [584.58 11 65.64632441]))
 
-
   (receive
-    '(10961 "hb"))
-
+    '(5863 "hb"))
 
   (receive
     (map-json-map
@@ -303,20 +400,20 @@
        "ts" 1511545528111,
        "cid" 1234}))
 
-
   (receive
     (map-json-map
-      {"event""info",
+      {"event" "info",
        "code" 20060,
        "msg" "Entering Maintenance mode."}))
+  [:resume ...]
+  [:reconnect ...]
 
 
   (receive
     (map-json-map
-      {"event""info",
+      {"event" "info",
        "code" 20017,
        "msg" "Foo"}))
-
 
   (receive
     (map-json-map
@@ -325,25 +422,17 @@
        "serverId" "90788dae-4b28-4f4f-963f-364c33e587d2",
        "platform" {"status" 1}}))
 
+  (receive
+    (map-json-map
+      {"event" "subscribed",
+       "pair" "ETHUSD"
+       "chanId" 5863}))
 
-  (do
-    (reset! CHANNELS {})
-    (receive
-      (map-json-map
-        {"event" "subscribed",
-         "pair" "ETHUSD"
-         "chanId" 5863})))
-
-
-  (do
-    (swap! CHANNELS assoc
-           5863 #exch.Ticker{:base :eth, :quote :usd})
-    (receive
-      (map-json-map
-        {"event" "unsubscribed",
-         "status" "OK",
-         "chanId" 5863})))
-
+  (receive
+    (map-json-map
+      {"event" "unsubscribed",
+       "status" "OK",
+       "chanId" 5863}))
 
   (receive
     (map-json-map
